afc2afa96dc12c079867e9291a18b7da9720996a
Paul Kiernan <paul.kiernan@reddit.com>
2020-12-10 21:50:55 -0800

    Updates terraform encryption scripts and values

--- scripts/encrypt_terraform_state.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eux
-
-REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
-
-sops \
-    --encrypt \
-    --in-place \
-    "${REPO_ROOT_DIR}/terraform/blueprints/paulynomial/terraform.tfstate"
