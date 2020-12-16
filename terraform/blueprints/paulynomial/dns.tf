26a2d0a7029270de6f70e08ccf3f147ecb90ca54
Paul Kiernan <paul.kiernan@reddit.com>
2020-12-15 20:51:23 -0800

    Starts tracking DNS records in terraform

---
+++ terraform/blueprints/paulynomial/dns.tf
@@ -0,0 +1,31 @@
+resource "digitalocean_domain" "paulynomial" {
+  name = "paulynomial.com"
+}
+
+resource "digitalocean_record" "paulynomial_NS1" {
+  domain = digitalocean_domain.paulynomial.name
+  type   = "NS"
+  name   = "@"
+  value  = "ns1.digitalocean.com"
+}
+
+resource "digitalocean_record" "paulynomial_NS2" {
+  domain = digitalocean_domain.paulynomial.name
+  type   = "NS"
+  name   = "@"
+  value  = "ns2.digitalocean.com"
+}
+
+resource "digitalocean_record" "paulynomial_NS3" {
+  domain = digitalocean_domain.paulynomial.name
+  type   = "NS"
+  name   = "@"
+  value  = "ns3.digitalocean.com"
+}
+
+resource "digitalocean_record" "root_paulynomial_A" {
+  domain = digitalocean_domain.paulynomial.name
+  type   = "A"
+  name   = "@"
+  value  = "104.236.6.204"
+}
