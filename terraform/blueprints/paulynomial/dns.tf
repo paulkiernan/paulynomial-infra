bb16ed76ee61abc15b898e6aef22de43ade361a8
Paul Kiernan <paul.kiernan@reddit.com>
2020-12-15 21:27:29 -0800

    Imports existing DNS records into terraform

--- terraform/blueprints/paulynomial/dns.tf
+++ terraform/blueprints/paulynomial/dns.tf
@@ -6,21 +6,21 @@ resource "digitalocean_record" "paulynomial_NS1" {
   domain = digitalocean_domain.paulynomial.name
   type   = "NS"
   name   = "@"
-  value  = "ns1.digitalocean.com"
+  value  = "ns1.digitalocean.com."
 }
 
 resource "digitalocean_record" "paulynomial_NS2" {
   domain = digitalocean_domain.paulynomial.name
   type   = "NS"
   name   = "@"
-  value  = "ns2.digitalocean.com"
+  value  = "ns2.digitalocean.com."
 }
 
 resource "digitalocean_record" "paulynomial_NS3" {
   domain = digitalocean_domain.paulynomial.name
   type   = "NS"
   name   = "@"
-  value  = "ns3.digitalocean.com"
+  value  = "ns3.digitalocean.com."
 }
 
 resource "digitalocean_record" "root_paulynomial_A" {
